4666f94f46dbee9896306ea56588111dd568487a
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-22 11:00:39 +0000

    Add project files.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Calculator App!");
+        }
+    }
+}

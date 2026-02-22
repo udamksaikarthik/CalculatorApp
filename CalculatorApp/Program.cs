f66a530fd97f89a2de4191e34e0f9e5eb71bfe60
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-22 11:04:15 +0000

    chore: title of the app and intro added

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Calculator App!");
+            Console.WriteLine("==== SIMPLE CALCULATOR ====\r\n\r\n1. Add\r\n2. Subtract\r\n3. Multiply\r\n4. Divide\r\n5. Exit");
         }
     }
 }

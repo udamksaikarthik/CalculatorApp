21752536851a9012e9285a5cfe870f911399c335
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-22 11:52:23 +0000

    feat: Operations added and functionality is working

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,29 +1,105 @@
-﻿
-namespace CalculatorApp
+﻿namespace CalculatorApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Calculator App!");
-            Console.WriteLine("==== SIMPLE CALCULATOR ====\r\n\r\n1. Add\r\n2. Subtract\r\n3. Multiply\r\n4. Divide\r\n5. Exit");
-            Console.WriteLine("Please select an option (1-5):");
+            bool appFlag = true;
+            while (appFlag)
+            {
+                Console.WriteLine("==== SIMPLE CALCULATOR ====\r\n\r\n1. Add\r\n2. Subtract\r\n3. Multiply\r\n4. Divide\r\n5. Exit");
+                Console.WriteLine("Please select an option (1-5):");
 
-            string? userOption = Console.ReadLine();
+                string? userOption = Console.ReadLine();
 
-            Console.WriteLine("userOption: " + userOption);
+                if (userOption != null)
+                {
+                    if (userOption.Equals("5"))
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation provided. App is closing now");
+                    Environment.Exit(0);
+                }
 
-            double firstNum = getNum("First");
 
-            Console.WriteLine("First Number: " + firstNum);
+                double firstNum = GetNum("First");
 
-            double secondNum = getNum("Second");
 
-            Console.WriteLine("Second Number: " + secondNum);
+                double secondNum = GetNum("Second");
 
+
+                double result = Operation(firstNum, secondNum, userOption);
+
+                Console.WriteLine("Result: " + result);
+
+                Console.WriteLine("Thank you for using Calculator App. Would you like to perform one more operation(yes/no)?");
+
+                string? appFlagTxt = Console.ReadLine();
+                if (appFlagTxt != null)
+                {
+                    appFlagTxt = appFlagTxt.ToLower();
+
+                    if (!appFlagTxt.Equals("yes") || !!appFlagTxt.Equals("no"))
+                    {
+                        Console.WriteLine("Invalid response provided. The app continues to run!");
+                    }
+
+
+                    if (appFlagTxt.Equals("yes"))
+                    {
+                        appFlag = false;
+                    }
+                }
+
+
+            }
         }
 
-        private static double getNum(string msg)
+        private static double Operation(double firstNum, double secondNum, string? userOption)
+        {
+            double result = 0;
+
+            switch (userOption)
+            {
+                case "1":
+                    result = firstNum + secondNum;
+                    break;
+                case "2":
+                    result = firstNum - secondNum;
+                    break;
+                case "3":
+                    result = firstNum * secondNum;
+                    break;
+                case "4":
+                    if (secondNum == 0)
+                    {
+                        Console.WriteLine("Division by zero is not possible. Please provide a valid number");
+                        secondNum = GetNum("Second");
+                        result = Operation(firstNum, secondNum, userOption);
+                    }
+                    else
+                    {
+                        result = firstNum / secondNum;
+                    }
+                    break;
+                case "5":
+                    Console.WriteLine("Exiting...");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+            }
+
+            return result;
+        }
+
+        private static double GetNum(string msg)
         {
             while (true)
             {
